d8c2a4e36c278eb9c64094f7b41621f91eb7df5f
Dan <danjsmall@gmail.com>
2025-02-11 00:41:07 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function (req, res){
+res.write("<html><head><meta charset='utf-8' /></head><body><p>¯\\_(ツ)_/¯</p></body></html>");
+	res.end();
+}
+).listen(3000);
+
+console.log("Server started on port 3000");
